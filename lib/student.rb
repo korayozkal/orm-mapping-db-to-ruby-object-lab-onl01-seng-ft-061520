115f50444ed712b92cf3be4020b1264a2cfabb0c
Koray Ozkal <koryozkal@gmail.com>
2020-07-22 02:02:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -78,14 +78,14 @@ def self.students_below_12th_grade
   end 
   
   
-   def self.first_X_students_in_grade_10(x)
+   def self.first_student_in_grade_10
   sql = <<-SQL
     SELECT * FROM students
     WHERE grade = 10
     SQL
   DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end.first(x)
+    end.first
   end 
   
   
