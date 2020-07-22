8dd81687f040c9e45b0fb13040696ed3f0f81b76
Koray Ozkal <koryozkal@gmail.com>
2020-07-22 01:49:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,20 +20,9 @@ class Student
     DB[:conn].execute(sql).collect do |row|
       self.new_from_db(row)
     end
-    
   end
 
-  def self.find_by_name(name)
-    sql = <<-SQL
-    SELECT * FROM students
-    WHERE name = ?
-    LIMIT 1
-    SQL
-    DB[:conn].execute(sql,name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
-end
+
   
   def save
     sql = <<-SQL
