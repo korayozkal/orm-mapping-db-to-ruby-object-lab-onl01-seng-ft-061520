59d9222a39f57d0da994d517d0ebf3c7b2c5a2d6
Koray Ozkal <koryozkal@gmail.com>
2020-07-22 01:53:41 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,8 +45,16 @@ class Student
   
   ###################
   
-  def self.
+  def self.all_students_in_grade_9
   
+    sql = <<-SQL
+    SELECT * FROM students
+    WHERE grade = 9
+    SQL
+    DB[:conn].execute(sql,name).map do |row|
+      self.new_from_db(row)
+    end
+  end
   
   
   
