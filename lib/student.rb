104334b7b5ca662871a11a77162f49d7f3ee30aa
Koray Ozkal <koryozkal@gmail.com>
2020-07-22 02:03:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,7 +88,7 @@ def self.students_below_12th_grade
     end.first
   end 
   
-  def self.first_student_in_grade_10
+  def self.all_students_in_grade_X
   sql = <<-SQL
     SELECT * FROM students
     WHERE grade = 10
